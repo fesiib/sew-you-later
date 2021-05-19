743a172af2d3312acb3b9ad39692a55cd58d5322
mhamzaerol <mehamerol@gmail.com>
2021-05-19 22:54:20 +0900

    Add image box and image with text

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
   return (
     <div>
       <Navbar />
-      <div style={{ textAlign: "center" }}>
+      <div>
         <div><h1>This is H1. Most probably one line.</h1></div>
 
         <div>
@@ -53,6 +53,13 @@ function App() {
 
         <div onClick={_increaseCounter}> +1 </div>
         <div onClick={_decreaseCounter}> -1 </div>
+
+        <img className="w-36 h-36 thumbnail" src="https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg" alt="example image"/>
+        
+        <div className="relative inline-block"> {/* Image box with over text */}
+          <img className="w-36 h-36 filter brightness-50 thumbnail" src="https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg" alt="example image"/>
+          <h3 className="text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">1+</h3>
+        </div> 
 
       </div>
     </div>
