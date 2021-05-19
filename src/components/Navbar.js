de406031f1bbab32b9d0c38b500e6342c3f950a8
Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>
2021-05-19 19:10:22 +0700

    init components folder

---
+++ src/components/Navbar.js
@@ -0,0 +1,159 @@
+import { Component } from "react";
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+
+const navigation = [
+    { name: 'New Orders', href: '#', current: true },
+    { name: 'Current Orders', href: '#', current: false },
+    { name: 'Previous Orders', href: '#', current: false },
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+class Navbar extends Component {
+    render() {
+        return (
+            <Disclosure as="nav" className="bg-indigo-900">
+                {({ open }) => (
+                    <>
+                        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                            <div className="relative flex items-center justify-between h-20">
+                                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                    {/* Mobile menu button*/}
+                                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-indigo-400 hover:text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                        <span className="sr-only">Open main menu</span>
+                                        {open ? (
+                                            <XIcon className="block h-6 w-6" aria-hidden="true" />
+                                        ) : (
+                                            <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                                        )}
+                                    </Disclosure.Button>
+                                </div>
+                                <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                                    <div className="flex-shrink-0 flex items-center">
+                                        <h1 className="text-white">Sew You Later!</h1>
+                                    </div>
+                                    <div className="hidden sm:block sm:ml-6">
+                                        <div className="flex px-4 space-x-4 ml-20">
+                                            {navigation.map((item) => (
+                                                <a
+                                                    key={item.name}
+                                                    href={item.href}
+                                                    className={classNames(
+                                                        item.current ? 'bg-indigo-900 text-white' : 'text-indigo-300 hover:bg-indigo-700 hover:text-white',
+                                                        'px-3 py-3 rounded-md text-sm'
+                                                    )}
+                                                    style={{ fontSize: "20px" }}
+                                                    aria-current={item.current ? 'page' : undefined}
+                                                >
+                                                    {item.name}
+                                                </a>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                                    {/* Profile dropdown */}
+                                    <Menu as="div" className="ml-3 relative">
+                                        {({ open }) => (
+                                            <>
+                                                <div>
+                                                    <Menu.Button className="bg-indigo-900 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-800 focus:ring-white">
+                                                        <span className="sr-only">Open user menu</span>
+                                                        <img
+                                                            className="h-12 w-12 rounded-full"
+                                                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                                            alt=""
+                                                        />
+                                                    </Menu.Button>
+                                                </div>
+                                                <Transition
+                                                    show={open}
+                                                    as={Fragment}
+                                                    enter="transition ease-out duration-100"
+                                                    enterFrom="transform opacity-0 scale-95"
+                                                    enterTo="transform opacity-100 scale-100"
+                                                    leave="transition ease-in duration-75"
+                                                    leaveFrom="transform opacity-100 scale-100"
+                                                    leaveTo="transform opacity-0 scale-95"
+                                                >
+                                                    <Menu.Items
+                                                        static
+                                                        className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                                    >
+                                                        <Menu.Item>
+                                                            {({ active }) => (
+                                                                <a
+                                                                    href="#"
+                                                                    className={classNames(
+                                                                        active ? 'bg-indigo-100' : '',
+                                                                        'block px-4 py-2 text-sm text-indigo-700'
+                                                                    )}
+                                                                >
+                                                                    Your Profile
+                                                                </a>
+                                                            )}
+                                                        </Menu.Item>
+                                                        <Menu.Item>
+                                                            {({ active }) => (
+                                                                <a
+                                                                    href="#"
+                                                                    className={classNames(
+                                                                        active ? 'bg-indigo-100' : '',
+                                                                        'block px-4 py-2 text-sm text-indigo-700'
+                                                                    )}
+                                                                >
+                                                                    Settings
+                                                                </a>
+                                                            )}
+                                                        </Menu.Item>
+                                                        <Menu.Item>
+                                                            {({ active }) => (
+                                                                <a
+                                                                    href="#"
+                                                                    className={classNames(
+                                                                        active ? 'bg-indigo-100' : '',
+                                                                        'block px-4 py-2 text-sm text-indigo-700'
+                                                                    )}
+                                                                >
+                                                                    Sign out
+                                                                </a>
+                                                            )}
+                                                        </Menu.Item>
+                                                    </Menu.Items>
+                                                </Transition>
+                                            </>
+                                        )}
+                                    </Menu>
+                                </div>
+                            </div>
+                        </div>
+
+                        <Disclosure.Panel className="sm:hidden">
+                            <div className="px-2 pt-2 pb-3 space-y-1">
+                                {navigation.map((item) => (
+                                    <a
+                                        key={item.name}
+                                        href={item.href}
+                                        className={classNames(
+                                            item.current ? 'bg-indigo-900 text-white' : 'text-indigo-300 hover:bg-indigo-700 hover:text-white',
+                                            'block px-3 py-2 rounded-md text-base font-medium'
+                                        )}
+                                        aria-current={item.current ? 'page' : undefined}
+                                    >
+                                        {item.name}
+                                    </a>
+                                ))}
+                            </div>
+                        </Disclosure.Panel>
+                    </>
+                )}
+            </Disclosure>
+        )
+    }
+}
+
+export default Navbar
