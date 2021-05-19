64fa992da993654390d0cc627220f08aa231b3b6
Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>
2021-05-19 19:09:02 +0700

    css <a> tag

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 // import './App.css';
 import './index.css';
 import { useDispatch, useSelector } from 'react-redux';
-import {increaseCounter, decreaseCounter } from './reducers/counter';
+import { increaseCounter, decreaseCounter } from './reducers/counter';
+
+import Navbar from './components/Navbar'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -12,7 +14,7 @@ function App() {
   const dispatch = useDispatch();
 
   const cnt = useSelector(state => state.counter.cnt);
-  
+
   const _increaseCounter = () => {
     dispatch(increaseCounter());
   };
@@ -27,29 +29,32 @@ function App() {
   };
 
   return (
-    <div style={{textAlign: "center"}}>
-      <div><h1>This is H1. Most probably one line.</h1></div>
-      
-      <div>
-        <button onClick={moveToSignin} className="red">move to sign in</button>
-        <button onClick={moveToSignin} className="green">same but green</button>
+    <div>
+      <Navbar />
+      <div style={{ textAlign: "center" }}>
+        <div><h1>This is H1. Most probably one line.</h1></div>
+
+        <div>
+          <button onClick={moveToSignin} className="red">move to sign in</button>
+          <button onClick={moveToSignin} className="green">same but green</button>
+        </div>
+
+        <form>
+          <label>
+            Name:
+          <input type="text" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
+        <br />
+
+        {cnt}
+
+        <div onClick={_increaseCounter}> +1 </div>
+        <div onClick={_decreaseCounter}> -1 </div>
+
       </div>
-      
-      <form>
-        <label>
-          Name:
-          <input type="text"/>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      
-      <br/>
-      
-      {cnt}
-      
-      <div onClick={_increaseCounter}> +1 </div>
-      <div onClick={_decreaseCounter}> -1 </div>
-      
     </div>
   );
 }
