23715c24dbd6891eb699d4873a30fec07927e26a
mhamzaerol <mehamerol@gmail.com>
2021-05-20 15:54:12 +0900

    Add new config for maxWidth class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
+      maxWidth: {
+        'xxs': '16rem',
+        'xxxs': '12rem',
+      },
       extend: {},
       fontFamily: {
         'sans': 'Work Sans, Work Sans, sans-serif',
