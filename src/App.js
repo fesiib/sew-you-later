8aff595f4176054f4af344c044973a12290158da
mhamzaerol <mehamerol@gmail.com>
2021-05-22 08:41:24 +0900

    Render Updates

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import SortBy from './components/SortBy';
 import SearchBar from './components/SearchBar';
 import SizeBar from './components/SizeBar';
 import ImageSearchTopBar from './components/ImageSearchTopBar';
+import DiscussionSearchPage from './pages/DiscussionSearchPage';
+import NewOrdersPage from './pages/NewOrdersPage';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -37,60 +39,79 @@ function App() {
     window.location = "/signin";
   };
 
+  // return (
+  //   <NewOrdersPage/>
+  // );
+
   return (
-    <div>
-      <Navbar />
-      <div>
-        <div><h1>This is H1. Most probably one line.</h1></div>
-
-        <div>
-          <button onClick={moveToSignin} className="red">move to sign in</button>
-          <button onClick={moveToSignin} className="green">same but green</button>
-        </div>
-
-        <form>
-          <label>
-            Name:
-          <input type="text" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-
-        <br />
-
-        {cnt}
-
-        <div onClick={_increaseCounter}> +1 </div>
-        <div onClick={_decreaseCounter}> -1 </div>
-
-        <img className="w-36 h-36 thumbnail" src="https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg" alt="example image"/>
-          <Notification position="bottom-right" size="h-6 w-6">
-            <ImageWithText/>
-          </Notification>
-          <div className="m-4">
-            <NewOrderItem/>
-          </div>
-        <div className="m-4">
-          <CurOrderItem/>
-        </div>
-        <div className="m-4">
-          <ProgressBar/>
-        </div>
-        <div className="flex justify-center">
-          <SortBy options={["Received Date", "A-Z", "Due Date", "Customer", "Location"]}/>
-        </div>
-        <div className="flex justify-center m-4">
-          <SearchBar/>
-        </div>
-        <div className="flex justify-center m-4">
-          <SizeBar/>
-        </div>
-        <div className="flex justify-center m-4">
-          <ImageSearchTopBar/>
-        </div>
-      </div>
-    </div>
+      <DiscussionSearchPage/>
   );
+
+  // return (
+  //   <div>
+  //     <Navbar />
+  //     <div>
+  //       <div><h1>This is H1. Most probably one line.</h1></div>
+
+  //       <div>
+  //         <button onClick={moveToSignin} className="red">move to sign in</button>
+  //         <button onClick={moveToSignin} className="green">same but green</button>
+  //       </div>
+
+  //       <form>
+  //         <label>
+  //           Name:
+  //         <input type="text" />
+  //         </label>
+  //         <input type="submit" value="Submit" />
+  //       </form>
+
+  //       <br />
+
+  //       {cnt}
+
+  //       <div onClick={_increaseCounter}> +1 </div>
+  //       <div onClick={_decreaseCounter}> -1 </div>
+
+  //       <img className="w-36 h-36 thumbnail" src="https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg" alt="example image"/>
+  //         <Notification position="bottom-right" size="h-6 w-6">
+  //           <ImageWithText/>
+  //         </Notification>
+  //         <div className="m-4">
+  //           <NewOrderItem/>
+  //         </div>
+  //       <div className="m-4">
+  //         <CurOrderItem/>
+  //       </div>
+  //       <div className="m-4">
+  //         <ProgressBar/>
+  //       </div>
+  //       <div className="flex justify-center">
+  //         <SortBy options={["Received Date", "A-Z", "Due Date", "Customer", "Location"]}/>
+  //       </div>
+  //       <div className="flex justify-center m-4">
+  //         <SearchBar/>
+  //       </div>
+  //       <div className="flex justify-center m-4">
+  //         <SizeBar/>
+  //       </div>
+  //       <div className="flex justify-center m-4">
+  //         <ImageSearchTopBar/>
+  //       </div>
+  //       <div className="gallery-small m-4">
+  //         <img className="thumbnail mx-4 mt-5" src="https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg"/>
+  //         <img className="thumbnail mx-4 mt-5" src="https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg"/>
+  //         <img className="thumbnail mx-4 mt-5" src="https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg"/>
+  //         <img className="thumbnail mx-4 mt-5" src="https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg"/>
+  //         <img className="thumbnail mx-4 mt-5" src="https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg"/>
+  //         <img className="thumbnail mx-4 mt-5" src="https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg"/>
+  //         <img className="thumbnail mx-4 mt-5" src="https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg"/>
+  //         <img className="thumbnail mx-4 mt-5" src="https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg"/>
+  //         <img className="thumbnail mx-4 mt-5" src="https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg"/>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default App;
