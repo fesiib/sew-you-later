08d85d0437a3999ebef6415277e6aa3ece515943
fesiib <tlekbay.b@gmail.com>
2021-05-22 18:50:48 +0900

    Margins

--- src/components/MeasurementMessage.js
+++ src/components/MeasurementMessage.js
@@ -4,7 +4,7 @@ const propVars = {
 
 function MeasurementMessage(props) {
     return (
-        <div className="card max-w-xl p-5">
+        <div className="card max-w-xl m-10">
             <textarea placeholder={propVars.measurementNotesPlaceholder} rows="15" className="resize-none shadow-md appearance-none rounded py-1 px-3 mt-3 w-full text-black"/>
         </div>
     );
