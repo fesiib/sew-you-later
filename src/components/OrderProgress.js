b92624724d363c07b34683c2b7fe833aca20bb87
fesiib <tlekbay.b@gmail.com>
2021-05-22 18:22:05 +0900

    Remove redundant stuff

--- src/components/OrderProgress.js
+++ src/components/OrderProgress.js
@@ -3,28 +3,13 @@ import { ExclamationCircleIcon } from '@heroicons/react/solid'
 import { CalendarIcon } from '@heroicons/react/outline';
 
 const propConst = {
-    refImagesTitle: "Reference Images",
-    orderDescTitle: "Order Description",
-    orderDetailsTitle: "Order Details",
     statusTitle: "Status: ",
     deadlineTitle: "Due: ",
 };
 
 const propVars = {
-    orderTitle: "T shirt with Pocket",
-    orderDesc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Phasellus condimentum commodo eros ac dapibus. 
-    Mauris convallis turpis ac turpis dapibus, at malesuada quam molestie. 
-    Mauris sed ex sagittis, dapibus mauris a, rhoncus felis. 
-    Proin tempus enim ac tincidunt luctus. Suspendisse eu turpis vel erat vehicula scelerisque eget at nisi. 
-    In odio lacus, laoreet vel rutrum eu, pharetra in quam. 
-    Quisque non tincidunt mauris. `,
     orderStatus: "Discussion",
     orderDeadline: "10 days left",
-    customerName: "Mehmet Hamza Erol",
-    customerInfo: "Male, 19",
-    customerLocation: "Korea/Daejeon",
-    customerEmail: "beyaldiz@kaist.ac.kr",
 };
 
 function OrderProgress(props) {
