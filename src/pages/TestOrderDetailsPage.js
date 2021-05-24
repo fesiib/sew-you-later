1630dbd7cdda370e73474334252641a31d4cbefd
mhamzaerol <mehamerol@gmail.com>
2021-05-24 17:05:22 +0900

    Added test page for order details page

---
+++ src/pages/TestOrderDetailsPage.js
@@ -0,0 +1,94 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { addCurOrder, removeCurOrder, updateCurOrder } from '../reducers/CurrentOrdersList';
+import { useState } from 'react';
+import OrderDetailsPage from './OrderDetailsPage';
+
+const propVars = {
+    orderTitle: "T shirt with Pocket",
+    customerName: "Mehmet Hamza Erol",
+    customerInfo: "Male, 19",
+    customerLocation: "Korea/Daejeon",
+    customerEmail: "beyaldiz@kaist.ac.kr",
+    hasNotification: true,
+    referenceImages: ["https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg", "https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg", "https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg"],
+    progressInfo: {    
+        estimatedDue: "2021-05-29", // YYYY-MM-DD
+        steps: ["Order Confirmation", "Customer's Response", "Discussion", "Measurement Record", "Production", "Delivery"],
+        curStep: {
+            stepIndex: 2,
+            stepStatus: "ongoing", // It will be either "incomplete", "ongoing", "complete". Also, the previous steps are always assumed to be "complete"!
+            nextStepDesc: "You will discuss stuff with the customer",
+            curStepDesc: "Discussion",
+        },
+        // orderConfirmation: {
+        //     str: "Order Confirmation",
+        //     status: "complete",
+        // },
+        // customerResponse: {
+        //     str: "Customer's Response",
+        //     status: "incomplete",
+        // },
+        // discussion: {
+        //     str: "Discussion",
+        //     status: "incomplete",
+        // },
+        // measurementRecord: {
+        //     str: "Measurement Record",
+        //     status: "incomplete",
+        // },
+        // production: {
+        //     str: "Production",
+        //     status: "incomplete",
+        // },
+        // delivery: {
+        //     str: "Delivery",
+        //     status: "incomplete",
+        // },
+    },
+    id: 0,
+};
+
+function TestOrderDetailsPage(props) {
+
+    // const [id, setId] = useState(0);
+
+    // const dispatch = useDispatch();
+
+    // const _addCurOrder = () => {
+    //     propVars.id = id;
+    //     setId(id + 1);
+    //     dispatch(addCurOrder(propVars));
+    // };
+
+    // const _removeCurOrder = () => {
+    //     if(id > 0) {
+    //         dispatch(removeCurOrder(id - 1));
+    //         setId(id - 1);
+    //     }
+    // };
+
+    // const _updateCurOrder = () => {
+    //     dispatch(updateCurOrder(id - 1, {
+    //         ...propVars, 
+    //         progressInfo: {
+    //             ...propVars.progressInfo, 
+    //             orderConfirmation: {
+    //                 ...propVars.progressInfo.orderConfirmation,
+    //                 status: "complete",
+    //             },
+    //         },
+    //     },));
+    // };
+
+    return (
+        // <div>
+        //     <div onClick={_addCurOrder}> +1 </div>
+        //     <div onClick={_removeCurOrder}> -1 </div>
+        //     <div onClick={_updateCurOrder}>U</div>
+        // </div>
+        <OrderDetailsPage vars={propVars}/>
+    );
+
+};
+
+export default TestOrderDetailsPage;
