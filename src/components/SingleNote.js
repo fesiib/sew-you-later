e390d094602c58afb30f34dd401219e2627a5c49
Baha Eren YALDIZ <beyaldiz17@gmail.com>
2021-05-24 23:44:47 +0900

    Add deletion function to report notes

--- src/components/SingleNote.js
+++ src/components/SingleNote.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
-import { addNote, updateDraftNoteBody, updateDraftNoteTitle } from '../reducers/imageNotes';
+import { addNote, deleteNote, updateDraftNoteBody, updateDraftNoteTitle } from '../reducers/imageNotes';
 
 const propVars = {
     notes: [
@@ -39,21 +39,18 @@ function SingleNote({id, imageId, reportId}) {
         dispatch(updateDraftNoteBody(id, e.target.value));
     }
 
+    const _deleteNote = () => {
+        dispatch(deleteNote(id));
+    }
+
     return (
         <div>
             {(() => {
                 if(reportId !== -1) {
-                    return (<div className="card mt-3 p-3 pr-0 grid grid-cols-12 divide-x w-full">
+                    return (<div className="card mt-3 p-3 pr-0 grid grid-cols-12 w-full">
                                 <div className="col-span-11">
                                     <input value={note ? note.title : title} className="shadow-md appearance-none rounded py-1 px-3 mb-3 w-5/6 text-black font-bold h2"/>
                                     <textarea value={note ? note.body: body} rows="2" className="resize-none shadow-md appearance-none rounded py-1 px-3 w-5/6 text-black"/>
-                                </div>
-                                <div className="py-8">
-                                    <button className="text-red-500 p-0 mx-2 shadow-none">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                        </svg>
-                                    </button>
                                 </div>
                             </div>);
                 }
@@ -70,7 +67,7 @@ function SingleNote({id, imageId, reportId}) {
                                     <textarea onChange={(e) => onChangeBody(e)} value={body} rows="2" className="resize-none shadow-md appearance-none rounded py-1 px-3 w-5/6 text-black"/>
                                 </div>
                                 <div className="py-8">
-                                    <button className="text-red-500 p-0 mx-2 shadow-none">
+                                    <button onClick={_deleteNote} className="text-red-500 p-0 mx-2 shadow-none">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                         </svg>
