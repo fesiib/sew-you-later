736bb7ee418fd50ad93c499d4ff87cdee6d4a8c7
Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>
2021-05-25 09:52:00 +0700

    Fix layout DiscussionNotesPage

--- src/pages/DiscussionNotesPage.js
+++ src/pages/DiscussionNotesPage.js
@@ -22,11 +22,13 @@ function DiscussionNotesPage(props) {
             <Navbar />
             <FAQButton />
             <Sidebar />
-            <div className="ml-20 mt-2 mr-8">
+            <div className="flex justify-between items-center ml-20 mt-8 mr-8">
                 <h3 className="font-bold mx-8">{propConst.viewNotesHeader}</h3>
                 <div className="flex justify-end">
                     <button className="bg-indigo-900 h-10" onClick={moveTo('discussion-search')}>{propConst.searchImagesButtonText}</button>
                 </div>
+            </div>
+            <div className="ml-20">
                 <div className="p-4 m-4 gallery-large">
                     <Notification type="check" position="top-right">
                         <img className="thumbnail border-4 border-green-400" src="https://www.istockphoto.com/resources/images/HomePage/Hero/1204187820.jpg" />
