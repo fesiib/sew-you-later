2d7db9076a7bdc8025864706158b914bead00686
Baha Eren YALDIZ <beyaldiz17@gmail.com>
2021-05-25 16:03:30 +0900

    Add are you sure to image delete

--- src/components/ImageNotes.js
+++ src/components/ImageNotes.js
@@ -1,8 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { addNote, deleteNote } from '../reducers/imageNotes';
 import { deleteImage } from '../reducers/reportImages';
+import Popup from 'reactjs-popup';
 
 import SingleNote from './SingleNote';
+import ConfirmCard from './ConfirmCard';
+
+const popupStyle = {width: "100%", height: "100%", backdropFilter: "blur(6px)", WebkitBackdropFilter: "blur(6px)", backgroundColor: "rgba(0,0,0,0.5)"}
 
 function ImageNotes({imageId, imageSrc, reportId, closePopup}) {
     const notes = useSelector(state => state.imageNotes.filter((note) => note.parentImageId === imageId));
@@ -19,6 +23,12 @@ function ImageNotes({imageId, imageSrc, reportId, closePopup}) {
         });
         closePopup();
     }
+
+    function popupClick(e, close) {
+        if(typeof e.target.className.includes === "function")
+            if(e.target.className.includes("back"))
+                close();
+    };
 
     return (
         <div className="max-w-5xl w-full lg:flex card overflow-hidden">
@@ -37,11 +47,33 @@ function ImageNotes({imageId, imageSrc, reportId, closePopup}) {
                             </div> 
                             <div className="grid grid-cols-2 divide-x content-center">
                                 <div>
-                                    <button onClick={_deleteImage} className="text-red-500 p-0 mx-2 shadow-none">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                        </svg>
-                                    </button>
+                                    <Popup
+                                        trigger={
+                                            <button className="text-red-500 p-0 mx-2 shadow-none">
+                                                <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                </svg>
+                                            </button>
+                                        }
+                                        modal
+                                        nested
+                                        position="center center"
+                                        contentStyle={popupStyle}
+                                    >
+                                        {close => (
+                                                    <div onClick={(e) => popupClick(e, close)} className="w-full h-full back">
+                                                        <ConfirmCard 
+                                                        onConfirm={_deleteImage} 
+                                                        onDecline={close} 
+                                                        title="Delete image?" 
+                                                        body="Deleting the image will permanently delete all the notes taken for this image."
+                                                        decline="Cancel"
+                                                        confirm="Delete"
+                                                    />
+                                                </div>
+                                            )
+                                        }
+                                    </Popup>
                                 </div>
                                 <div>
                                     <button onClick={closePopup} className="text-black p-0 mx-2 shadow-none">
