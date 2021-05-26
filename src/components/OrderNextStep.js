fbcae5d6770013eb0344ed9f89d0a0ad72968eb6
Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>
2021-05-26 12:05:43 +0700

    Modify next step button onClick and some optimization

--- src/components/OrderNextStep.js
+++ src/components/OrderNextStep.js
@@ -17,68 +17,78 @@ const propConst = {
 
 function OrderNextStep(props) {
     const orderId = new URLSearchParams(window.location.search).get('orderId');
-   
+
     const dispatch = useDispatch();
 
     const updateTheOrder = () => {
+        const curPage = props.vars.nextStepPage
         dispatch(updateCurOrder(updateProgress(), props.vars.id));
+        window.location = "/" + curPage + "?orderId=" + orderId;
     };
 
     const updateProgress = () => {
-        const curStepPage = props.vars.nextStepPage;
-        const curStatus = props.vars.curStepStatus;
+        // const curStepPage = props.vars.nextStepPage;
+        const curStep = props.vars.curStepIndex;
+
         const descArray = {
-            "discussion-search": {
-                "incomplete": [
-                    "Having a discussion with the customer",
-                    "You have to complete notes taking with the customer so that you can proceed to the measurements part.",
-                ],
-                "complete": [
-                    "About to create a measurements form",
-                    "You will create a measurements form that will be sent to the customer by choosing the body parts that you need the measurements.",
-                ],
-            },
-            "order-measurements": {
-                "incomplete": [
-                    "Waiting for the customer's response for the measurements",
-                    "You will get the customer's measurements after the customer fills in the form. If you have not sent the form yet, please do so by clicking the arrow above!",
-                ],
-                "complete": [
-                    "Under production",
-                    "You can send your first progress report to the customer so they can see the updates. Click the arrow when you are ready!",
-                ]
-            },
-            "order-reports": {
-                "incomplete": [
-                    "Under production",
-                    "Any updates? Click the arrow above to start sending progress report to the customer.",
-                ]
-            },
+            // Discussion -> Measurement Record
+            // Measurement Record -> Customer's Response
+            1: [
+                "About to create a measurements form",
+                `You will create a measurements form that will be sent to the customer by choosing 
+                the body parts that you need the measurements. Feel free to add any additional
+                notes to the discussion page anytime by clicking "Discussion Notes" on the left panel.`,
+                curStep + 1,
+                "order-measurements"
+            ],
+            // 2 shouldn't have any effect since it is toggled by measurement page's Send button
+            2: [
+                props.vars.curStepDesc,
+                props.vars.nextStepDesc,
+                curStep,
+                "order-measurements"
+            ],
+            // Waiting measurements from the customer
+            3: [
+                props.vars.curStepDesc,
+                props.vars.nextStepDesc,
+                curStep,
+                "order-measurements"
+            ],
+            // This one should be integrated with the automatic measurements fill-in.
+            // Not supposed to be here
+            // Customer's Response -> Production
+            4: [
+                "Under production",
+                `Any updates on the product? Click the arrow above to start sending progress report 
+                to the customer.`,
+                curStep,
+                "order-reports"
+            ],
         }
-        if (curStatus === "ongoing") {
-            return props.vars
-        }
-        else {
-            return {
-                ...props.vars,
-                curStepStatus: "ongoing",
-                curStepDesc: descArray[curStepPage][curStatus][0],
-                nextStepDesc: descArray[curStepPage][curStatus][1],
-            }
+        return {
+            ...props.vars,
+            curStepStatus: "ongoing",
+            curStepDesc: descArray[curStep][0],
+            nextStepDesc: descArray[curStep][1],
+            curStepIndex: descArray[curStep][2],
+            nextStepPage: descArray[curStep][3],
         }
     }
 
     return (
-        <div className="my-10 grid grid-rows-2 grid-cols-3 place-content-evenly bg-white rounded-xl">
-            <div className="m-5 row-span-1 col-span-2 row-start-1 flex flex-row items-center">
-                <h1 className="">{propConst.nextStepTitle}</h1>
+        <div className="my-10 bg-white rounded-xl space-y-4">
+            <div className="flex items-center justify-between mx-5">
+                <div className="flex">
+                    <h1 className="">{propConst.nextStepTitle}</h1>
+                </div>
+                <div className="flex cursor-pointer">
+                    <a onClick={() => updateTheOrder()} className="text-green-500 hover:text-green-600">
+                        <ArrowCircleRightIcon className="h-16 mt-3" />
+                    </a>
+                </div>
             </div>
-            <div className="ml-5 row-span-1 col-span-1 cursor-pointer justify-end">
-                <a onClick={() => updateTheOrder()} href={"/" + props.vars.nextStepPage + "?orderId=" + orderId} className="text-green-500">
-                    <ArrowCircleRightIcon className="h-16 mt-3" />
-                </a>
-            </div>
-            <div className="mx-5 pb-3 col-span-3 row-span-1 row-start-2">
+            <div className="mx-5 pb-3">
                 <h2 className="mb-1">{props.vars.steps.split('-')[props.vars.curStepIndex]}</h2>
                 <p>{props.vars.nextStepDesc}</p>
             </div>
