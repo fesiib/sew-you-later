179ea2dc01b7b8e750f23c60832304d3b7bba3e1
Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>
2021-05-26 20:18:36 +0700

    Static-size SortBy drop down

--- src/components/SortBy.js
+++ src/components/SortBy.js
@@ -24,13 +24,13 @@ function SortBy(props) {
     };
     
     return (
-        <div className="flex flex-col z-10">
+        <div className="flex flex-col z-10 w-48">
             <p className="mb-0.5 text-xs">{propConst.desc}</p>
-            <Menu as="div" className="relative max-w-max text-left">
+            <Menu as="div" className="relative w-full text-left">
             {({ open }) => (
                 <>
                 <div>
-                    <Menu.Button className="inline-flex justify-center w-full rounded-sm border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                    <Menu.Button className="inline-flex justify-between w-full rounded-sm border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
                     {chosenOption}
                     <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
                     </Menu.Button>
