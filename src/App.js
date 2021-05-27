c6d4edec79e3bb6089c48ffbe29951c9b1ba8815
fesiib <tlekbay.b@gmail.com>
2021-05-28 01:57:47 +0900

    firebase.json update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 // import logo from './logo.svg';
 // import './App.css';
 import './index.css';
-import { Redirect } from 'react-router-dom';
 
 function App() {
-  return (<div>Hello</div>);
-  return (<Redirect to={"/new-orders"}/>);
-  
+  return (<div>Hello, please go to /new-orders </div>);
 }
 
 export default App;
