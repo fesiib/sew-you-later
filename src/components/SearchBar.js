d22c3623b02cff6dded3e2f49685c18ea82972a3
mhamzaerol <mehamerol@gmail.com>
2021-05-28 19:48:10 +0900

    Fix placeholder for searchbar and add error logging

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,10 +4,16 @@ import { useState } from 'react';
 
 function SearchBar(props) {
 
+    const defaultPlaceholder = "Search for images here";
+    const errorPlaceholder = "Error: Problem Occured While Fetching Images :(";
     const [textInput, setTextInput] = useState("");
+    const [placeholder, setPlaceHolder] = useState(defaultPlaceholder);
     var searchButton;
 
     const handleEvent = (e) => {
+        if(placeholder == errorPlaceholder) {
+            setPlaceHolder(defaultPlaceholder);
+        }
         setTextInput(e.target.value);
     };
 
@@ -22,14 +28,18 @@ function SearchBar(props) {
         const url = `http://pilgrim.wtf:25565/?search=${textInput} clothes`;
         fetch(url).then(response => {
             response.json().then(data => {
-                setTextInput("");
+                setPlaceHolder(defaultPlaceholder);
                 props.setSearchResults(data.filter((val, index) => index < 20));
-            })  
+            }).catch(reason => {
+                setPlaceHolder(errorPlaceholder);
+                console.log(reason);
+            });
         }).catch(reason => {
-            setTextInput("Error: Problem Occured While Fetching Images :(");
+            setPlaceHolder(errorPlaceholder);
             console.log(reason);
         });
-        setTextInput("Searching...");
+        setTextInput("");
+        setPlaceHolder("Searching...");
     };
 
     const makeSearch = () => {
@@ -38,7 +48,7 @@ function SearchBar(props) {
 
     return (
         <div className="flex">
-            <input className="flex-1 w-120 p-2 rounded-lg rounded-r-none focus:outline-none" type="text" placeholder="Search for images here" value={textInput} onChange={handleEvent} onKeyPress={checkEnter}/>
+            <input className="flex-1 w-120 p-2 rounded-lg rounded-r-none focus:outline-none" type="text" placeholder={placeholder} value={textInput} onChange={handleEvent} onKeyPress={checkEnter}/>
             <button ref={button => searchButton = button} className="flex-shrink-0 bg-white rounded-l-none rounded-lg" onClick={() => {makeSearch()}}>    
                 <SearchIcon className="text-gray-600 bg-white h-5 w-5"/>
             </button>
