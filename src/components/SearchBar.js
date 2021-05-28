78d911db400db7d7b937297b7a6741ee035d14c5
mhamzaerol <mehamerol@gmail.com>
2021-05-28 19:37:23 +0900

    discussion page, press enter and perform search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,10 +5,18 @@ import { useState } from 'react';
 function SearchBar(props) {
 
     const [textInput, setTextInput] = useState("");
+    var searchButton;
 
     const handleEvent = (e) => {
         setTextInput(e.target.value);
     };
+
+    const checkEnter = (e) => {
+        const code =  e.keyCode || e.which;;
+        if(code == 13) { // enter key
+            searchButton.click();
+        }  
+    }
 
     async function search()  {
         const url = `http://pilgrim.wtf:25565/?search=${textInput} clothes`;
@@ -16,11 +24,11 @@ function SearchBar(props) {
             response.json().then(data => {
                 setTextInput("");
                 props.setSearchResults(data.filter((val, index) => index < 20));
-            }).catch(reason => {
-                setTextInput("Error: Problem Occured While Fetching Images :(");
-                console.log(reason);
-            });  
-        })
+            })  
+        }).catch(reason => {
+            setTextInput("Error: Problem Occured While Fetching Images :(");
+            console.log(reason);
+        });
         setTextInput("Searching...");
     };
 
@@ -30,8 +38,8 @@ function SearchBar(props) {
 
     return (
         <div className="flex">
-            <input className="flex-1 w-120 p-2 rounded-lg rounded-r-none focus:outline-none" type="text" placeholder="Search for images here" value={textInput} onChange={handleEvent}/>
-            <button className="flex-shrink-0 bg-white rounded-l-none rounded-lg" onClick={() => {makeSearch()}}>    
+            <input className="flex-1 w-120 p-2 rounded-lg rounded-r-none focus:outline-none" type="text" placeholder="Search for images here" value={textInput} onChange={handleEvent} onKeyPress={checkEnter}/>
+            <button ref={button => searchButton = button} className="flex-shrink-0 bg-white rounded-l-none rounded-lg" onClick={() => {makeSearch()}}>    
                 <SearchIcon className="text-gray-600 bg-white h-5 w-5"/>
             </button>
         </div>
